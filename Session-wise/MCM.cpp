bb99ef688fa4653172a61571d2a890e5aebee246
bharatmudgil0 <mudgilbharat0@gmail.com>
2026-04-08 12:18:22 +0530

    Matrix Chain Multiplication

---
+++ Session-wise/MCM.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minMultRec(vector<int> &arr, int i, int j, vector<vector<int>> &memo)
+{
+    if (i + 1 == j)
+        return 0;
+
+    if (memo[i][j] != -1)
+        return memo[i][j];
+
+    int res = INT_MAX;
+
+    for (int k = i + 1; k < j; k++)
+    {
+        int curr = minMultRec(arr, i, k, memo) + minMultRec(arr, k, j, memo) + arr[i] * arr[k] * arr[j];
+
+        res = min(curr, res);
+    }
+
+    memo[i][j] = res;
+    return res;
+}
+
+int matrixMultiplication(vector<int> &arr)
+{
+
+    int n = arr.size();
+    vector<vector<int>> memo(n, vector<int>(n, -1));
+    return minMultRec(arr, 0, n - 1, memo);
+}
+
+int main()
+{
+    vector<int> arr = {2, 1, 3, 4};
+    int res = matrixMultiplication(arr);
+    cout << res << endl;
+    return 0;
+}
